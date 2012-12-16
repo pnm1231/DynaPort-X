4fbf806f547c98c1e169c5f3e1b37bf6d39ce7f1
pnm123 <prasad.n@dynamiccodes.com>
2012-12-16 18:47:41 +0530

    version 0.2.2 - cURL DELETE method

--- system/libs/cURL.php
+++ system/libs/cURL.php
@@ -5,18 +5,26 @@ class cURL {
     private $ch;
 
     function __construct(){
-        // is cURL installed yet?
+        /*
+         * Check if cURL is installed
+         */
         if(!function_exists('curl_init')){
             new Error_Controller('Sorry cURL is not installed!');
         }
         
-        // OK cool - then let's create a new cURL resource handle
+        /*
+         * Create a new cURL resource handle
+         */
         $this->ch = curl_init();
         $this->setReturnType();
     }
     
+    /**
+     * Set the URL
+     * 
+     * @param string $url URL to access
+     */
     function setUrl($url){
-        // Set URL to download
         $url = filter_var($url,FILTER_SANITIZE_URL);
         if(!empty($url)){
             curl_setopt($this->ch,CURLOPT_URL,$url);
@@ -25,28 +33,68 @@ class cURL {
         }
     }
     
+    /**
+     * Set the HTTP method to POST
+     * 
+     * @param string $body POST fields
+     */
+    function setHttpPost($body){
+        curl_setopt($this->ch,CURLOPT_POST,true);
+        curl_setopt($this->ch,CURLOPT_POSTFIELDS,$body);
+    }
+    
+    /**
+     * Set the HTTP method to DELETE
+     * 
+     * @param string $body DELETE fields
+     */
+    function setHttpDelete($body){
+        curl_setopt($this->ch,CURLOPT_CUSTOMREQUEST,'DELETE');
+        curl_setopt($this->ch,CURLOPT_POSTFIELDS,$body);
+    }
+    
+    /**
+     * Set the Referring URI
+     * 
+     * @param string $referer Referring URI
+     */
     function setReferer($referer){
-        // Set a referer
         curl_setopt($this->ch,CURLOPT_REFERER,$referer);
     }
     
+    /**
+     * Set a custom User Agent
+     * 
+     * @param string $userAgent User agent name
+     */
     function setUserAgent($userAgent){
-        // User agent
         curl_setopt($this->ch,CURLOPT_USERAGENT,$$userAgent);
     }
     
+    /**
+     * Include header in the result?
+     * 
+     * @param boolean $header true/false
+     */
     function setInclHeader($header=0){
-        // Include header in result? (0 = yes, 1 = no)
         curl_setopt($this->ch,CURLOPT_HEADER,$header);
     }
     
+    /**
+     * Should cURL return or print out the data?
+     * 
+     * @param boolean $return true=return/false=print
+     */
     function setReturnType($return=1){
-        // Should cURL return or print out the data? (true = return, false = print)
         curl_setopt($this->ch,CURLOPT_RETURNTRANSFER,$return);
     }
     
+    /**
+     * Timeout of the cURL request
+     * 
+     * @param int $timeout Timeout in seconds
+     */
     function setTimeout($timeout){
-        // Timeout in seconds
         if(is_numeric($timeout)){
             curl_setopt($this->ch,CURLOPT_TIMEOUT,$timeout);
         }else{
@@ -54,11 +102,20 @@ class cURL {
         }
     }
     
+    /**
+     * Output (default: return) the result
+     * 
+     * @return mixed The result
+     */
     function output(){
-        // Download the given URL, and return output
+        /*
+         * Download the given URL, and return output
+         */
         $output = curl_exec($this->ch);
         
-        // Close the cURL resource, and free system resources
+        /*
+         * Close the cURL resource, and free system resources
+         */
         curl_close($this->ch);
         
         return $output;
