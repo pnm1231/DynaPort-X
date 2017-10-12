3af0c04a4da4d98bdb8c6cc090cd2cd733251215
Prasad Nayanajith <lanka1st.com@gmail.com>
2017-10-12 11:50:45 +0530

    v2.0.86 - Minor fixes

--- application/modules/common/views/footer.php
+++ application/modules/common/views/footer.php
@@ -3,7 +3,7 @@
     
     <footer>
         <div id="copyright">
-            Copyright &copy; 2017
+            Copyright &copy; <?=date('Y')?>
         </div>
         <div class="note">This view is located at: application/modules/common/views/footer.php</div>
     </footer>
